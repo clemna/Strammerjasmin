c624ba265f095817cd639e3b8ed6e978a01f3a20
Clemens <clemensspitzer@gmx.at>
2020-11-26 15:22:30 +0100

    + looting
    added looting

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed = 20f;
+    [SerializeField]
+    private Vector3 axis = Vector3.forward;
+
+    void Update()
+    {
+        transform.Rotate(axis, rotationSpeed * Time.deltaTime);
+    }
+}
