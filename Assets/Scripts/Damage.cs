4a4213c5e19f67dfe21bef1fd1ac18fdfc8ad338
Clemens <clemensspitzer@gmx.at>
2020-11-27 01:03:06 +0100

    + enemy and dmg
    added dmg script + particle effect

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Damage : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject hitEffect = null;
+
+    private Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Health health = other.transform.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage();
+        }
+
+        float offset = rigid.velocity.magnitude;
+        GameObject.Instantiate(hitEffect, transform.position, transform.rotation * Quaternion.Euler(Vector3.up * 180f));
+
+    }
+}
