22886e46dec1c8ec91263e2367d9cddb959ef249
Clemens <clemensspitzer@gmx.at>
2021-01-07 22:59:03 +0100

    modified Player and OneWayPlatform

---
+++ Assets/Scripts/OneWay.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+[RequireComponent(typeof(Collider2D))]
+
+public class OneWayController : MonoBehaviour
+{
+
+    public String m_OneWayTag = "OneWayPlatform"; //Tag to use for one way platforms
+
+    List<Collider2D> m_ActorColliders; //List of colliders on this character
+
+    void Awake()
+    {
+        //Cache refrences to this character's colliders in a list
+        m_ActorColliders = new List<Collider2D>();
+
+        Collider2D[] colliders = GetComponents<Collider2D>();
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+
+            //Exclude the one way trigger from the collider list
+            if (!colliders[i].isTrigger)
+                m_ActorColliders.Add(colliders[i]);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //When a platform with the one way tag enters the trigger
+        //ignore collisions between it and this character
+        if (other.tag == m_OneWayTag)
+        {
+
+            for (int i = 0; i < m_ActorColliders.Count; i++)
+            {
+
+                Physics2D.IgnoreCollision(other, m_ActorColliders[i], true);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //When the platform exits the trigger re-enable collisions
+        if (other.tag == m_OneWayTag)
+        {
+
+            for (int i = 0; i < m_ActorColliders.Count; i++)
+            {
+
+                Physics2D.IgnoreCollision(other, m_ActorColliders[i], false);
+            }
+        }
+    }
+}
