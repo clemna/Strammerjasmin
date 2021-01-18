4c4f7c79eda2f2cc309df11f2477db754c042973
Clemens <clemensspitzer@gmx.at>
2021-01-18 11:49:31 +0100

    adjusted movement for animations, pause menu buttons

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
 
     public CharacterController2D controller;
 
+    public Animator animator;
+
     public float runSpeed = 40f;
 
     private float lastDirection = 0;
@@ -26,6 +28,7 @@ public class PlayerMovement : MonoBehaviour
     {
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Walk", Mathf.Abs(horizontalMove));
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
             lastDirection = 1;
