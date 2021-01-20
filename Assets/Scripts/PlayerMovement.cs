3c8490d26af043b726bfaa3a2c7f6826f4800eb9
Clemens <clemensspitzer@gmx.at>
2021-01-20 11:53:58 +0100

    adjusted sound

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -30,7 +30,7 @@ public class PlayerMovement : MonoBehaviour
     {
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        if (Mathf.Abs(horizontalMove) > 0 && audiomanager != null)
+        if (Mathf.Abs(horizontalMove) > 0 && audiomanager)
         {
             audiomanager.Play("Steps");
         }
