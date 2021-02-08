f86afb6dca3b42b55d0229ad0f7ed67e701e175b
Clemens <clemensspitzer@gmx.at>
2021-02-08 18:04:13 +0100

    fixed footsteps

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    CharacterController2D cc;
+    Rigidbody2D rb;
+    AudioSource aud;
+    void Start()
+    {
+        cc = GetComponent<CharacterController2D>();
+        rb = GetComponent<Rigidbody2D>();
+        aud = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cc.m_Grounded == true && rb.velocity.magnitude > 2f && aud.isPlaying == false)
+        {
+            aud.Play();
+        }
+    }
+}
