34913d7be54d0cede655c296bd0cc0b68aa91a6e
Clemens <clemensspitzer@gmx.at>
2021-02-12 03:00:33 +0100

    fixed videos

---
+++ Assets/Scripts/IntroController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroController : MonoBehaviour
+{
+    public GameObject Continue;
+    public LevelLoader Loader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitContinue());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Loader.LoadNextLevel();
+        }
+    }
+
+    IEnumerator WaitContinue()
+    {
+        yield return new WaitForSeconds(5);
+
+        Continue.SetActive(true);
+
+    }
+}
